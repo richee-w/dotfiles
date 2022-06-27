fda86cec71a22f6a6236223b555b1964a15ab73a
richee-w <contact@richeewilson.com>
2022-06-27 16:44:44 +0100

    add custom omb files

---
+++ .omb-custom/aliases.sh
@@ -0,0 +1,10 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+alias sudo='sudo '
+alias vi='nvim'
+alias vim='nvim'
+alias pacU='sudo pacman -Syyu'
+alias yayU='yay -Syyu'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias config-list='config log --pretty=format: --name-only --diff-filter=A | sort -'
+# alias emacs="emacsclient -c -a 'emacs'"
