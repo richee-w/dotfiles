1d4f2aca86ba1ef18010802db87bd1cb45132aa4
richee-w <contact@richeewilson.com>
2022-07-01 11:49:12 +0100

    aliases.sh update

--- .omb-custom/aliases.sh
+++ .omb-custom/aliases.sh
@@ -1,8 +1,8 @@
 # Add your own custom alias in the custom/aliases directory. Aliases placed
 # here will override ones with the same name in the main alias directory.
 alias sudo='sudo '
-alias vi='nvim'
-alias vim='nvim'
+# alias vi='nvim'
+# alias vim='nvim'
 alias pacU='sudo pacman -Syyu'
 alias yayU='yay -Syyu'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
