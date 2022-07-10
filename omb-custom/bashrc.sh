aac19566d236391463b9ec4957f13d2302ea706d
richee-w <contact@richeewilson.com>
2022-07-10 21:32:46 +0100

    minor correction to comments

--- omb-custom/bashrc.sh
+++ omb-custom/bashrc.sh
@@ -1,5 +1,5 @@
-# Add your own custom alias in the custom/aliases directory. Aliases placed
-# here will override ones with the same name in the main alias directory.
+# Add your own custom .bashrc mods here.
+
 alias sudo='sudo '
 alias vi='nvim'
 alias vim='nvim'
@@ -8,5 +8,4 @@ alias yayU='yay -Syyu'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias config-list='config log --pretty=format: --name-only --diff-filter=A | sort -'
 alias code='codium'
-# alias emacs="emacsclient -c -a 'emacs'"
 # alias emacs="emacsclient -c -a 'emacs'"
