f5bfe6c78977f8abcb3ef6490f54ff72e1736cef
richee-w <contact@richeewilson.com>
2022-07-10 16:53:27 +0100

    update bashrc.sh

--- omb-custom/bashrc.sh
+++ omb-custom/bashrc.sh
@@ -7,4 +7,6 @@ alias pacU='sudo pacman -Syyu'
 alias yayU='yay -Syyu'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias config-list='config log --pretty=format: --name-only --diff-filter=A | sort -'
+alias code='codium'
+# alias emacs="emacsclient -c -a 'emacs'"
 # alias emacs="emacsclient -c -a 'emacs'"
